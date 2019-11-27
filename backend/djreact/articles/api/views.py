d8fb98f0290780a6d16ba4db437424fad906cec5
Wojtek Owczarek <wojciech.owczarek92@gmail.com>
2019-11-27 19:02:13 +0100

    setup API, created Article model, serializers, and generic views for api

---
+++ backend/djreact/articles/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from serializers import ArticleSerializer
+
+
+from articles.models import Article
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetailView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
